73bebade30ec9c9beaf229765bc0a4a6a387d3cd
joaovitoras <joaovitor_as08802@hotmail.com>
2022-06-25 17:47:32 -0300

    Change scope

--- config/initializers/slack_ruby_bot_server.rb
+++ config/initializers/slack_ruby_bot_server.rb
@@ -2,5 +2,5 @@
 
 SlackRubyBotServer.configure do |config|
   config.oauth_version = :v2
-  config.oauth_scope = ['users:read', 'channels:read', 'groups:read', 'chat:write', 'commands', 'incoming-webhook']
+  config.oauth_scope = ['users:read', 'chat:write']
 end
