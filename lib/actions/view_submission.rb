e472c82cc7a0e04952a6ee0be6a26bfa53eea9bb
joaovitoras <joaovitor_as08802@hotmail.com>
2022-06-29 10:05:25 -0300

    Check data

--- lib/actions/view_submission.rb
+++ lib/actions/view_submission.rb
@@ -7,11 +7,13 @@ SlackRubyBotServer::Events.configure do |config|
     team = Team.where(team_id: payload[:team][:id]).first || raise("Cannot find team with ID #{payload[:team][:id]}.")
     slack_client = Slack::Web::Client.new(token: team.token)
 
-    team.mails.create(
+    mail_data = {
       from_user_id: payload[:user][:id],
       to_user_id: payload[:view][:state][:values][:user_selected][:'users_select-action'][:selected_user],
       text: payload[:view][:state][:values][:message][:content][:value]
-    )
+    }
+    puts mail_data
+    team.mails.create(mail_data)
 
     Helpers::DisplayHome.perform(slack_client, team, payload[:user][:id])
     nil
